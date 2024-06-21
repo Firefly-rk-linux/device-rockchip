9e318091807138f42fdeadfdd008edc20bf1f9f8
lyj <lyj@t-chip.com.cn>
2024-06-21 16:43:26 +0800

    add aio-3588jd4 mk file

---
+++ rk3588/aio-3588jd4-buildroot.mk
@@ -0,0 +1,10 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/itx-3588j-buildroot.mk
+
+# Kernel dts
+export RK_KERNEL_DTS=aio-3588jd4
+
+# PRODUCT MODEL
+export RK_PRODUCT_MODEL=AIO-3588JD4
