ae5d52cf45207244644ee89ca9633507d4c08556
maocl <mcl@t-chip.com.cn>
2024-12-17 11:29:07 +0800

    add B2-3588j-main-debian.mk and B2-3588j-main-ubuntu.mk

---
+++ rk3588/B2-3588j-main-debian.mk
@@ -0,0 +1,21 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+DEVICE_NAME=$(echo ${CMD} | awk -F '/' '{print $NF}')
+DEVICE_NAME=${DEVICE_NAME%\-*}
+source $CUR_DIR/${DEVICE_NAME}-ubuntu.mk
+
+# rootfs_system
+export RK_ROOTFS_SYSTEM=debian
+
+export RK_ROOTFS_IMG=debian/rootfs.img
+
+# recovery ramdisk
+#export RK_RECOVERY_RAMDISK=
+
+# Recovery config
+#export RK_CFG_RECOVERY=firefly_rk3588_abmc_main_recovery
+
+# parameter for GPT table
+#export RK_PARAMETER=abmc_main_parameter-fit.txt
+
---
+++ rk3588/B2-3588j-main-ubuntu.mk
@@ -0,0 +1,35 @@
+CMD=`realpath $BASH_SOURCE`
+CUR_DIR=`dirname $CMD`
+
+source $CUR_DIR/itx-3588j-buildroot.mk
+
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT="$RK_KERNEL_DEFCONFIG_FRAGMENT"
+
+export FF_EXTBOOT_SIZE=256M
+
+# Kernel dts
+export RK_KERNEL_DTS=B2-3588j-main
+
+# Set rootfs type, including ext2 ext4 squashfs
+export RK_ROOTFS_TYPE=ext4
+# rootfs image path
+export RK_ROOTFS_IMG=ubuntu_rootfs/rootfs.img
+
+# Buildroot config
+export RK_CFG_BUILDROOT=
+
+#OEM config
+export RK_OEM_DIR=
+
+#userdata config
+export RK_USERDATA_DIR=
+
+# rootfs_system
+export RK_ROOTFS_SYSTEM=ubuntu
+
+# packagefile for make update image
+export RK_PACKAGE_FILE=rk3588-ubuntu-package-file
+
+#SPL_CONFIG
+export RK_SPL_INI_CONFIG=RK3588_115200MINIALL.ini
